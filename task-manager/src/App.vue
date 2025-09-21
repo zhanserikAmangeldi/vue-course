677b56e7cc1b725ad507006fffb2fc2a23b9988a
zhanserikAmangeldi <amangeldi.janserik2017@gmail.com>
2025-09-21 20:28:15 +0500

    add button to show/unshow completed tasks

--- task-manager/src/App.vue
+++ task-manager/src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 let id = 0
+
 const newTask = ref('')
+const hideCompleted = ref(false)
 const tasks = ref([
   { id: id++, desc: "First Task", done: true },
   { id: id++, desc: "Second Task", done: false },
   { id: id++, desc: "Third Task", done: false }
 ])
+
+const completedTasks = computed(() => {
+  return hideCompleted.value
+    ? tasks.value.filter((task) => !task.done)
+    : tasks.value
+})
 
 function addTask() {
   tasks.value.push({
@@ -32,12 +40,15 @@ function removeTask(id) {
     <button>Add the task</button>
   </form>
   <ul>
-    <li v-for="task in tasks" :key="task.id">
+    <li v-for="task in completedTasks" :key="task.id">
       <input type="checkbox" v-model="task.done">
       <span :class="{ done: task.done}">{{ task.desc }}</span>
       <button @click="removeTask(task.id)">x</button>
     </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed'}}
+  </button>
 </template>
 
 <style scoped>
